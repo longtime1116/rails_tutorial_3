97b2944cd430eac6f2c547a89595f65e05c4ddfd
longtime1116 <longtime1116@gmail.com>
2018-05-12 20:16:22 +0900

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title(page_title = nil)
+    base_title = "Rails Tutorial"
+    return base_title if page_title.blank?
+    page_title + " | " + base_title
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Rails Tutorial"
+    assert_select "title", "Rails Tutorial"
   end
 
   test "should get help" do
@@ -22,6 +22,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | Rails Tutorial"
+    assert_select "title", "Rails Tutorial"
   end
 end
